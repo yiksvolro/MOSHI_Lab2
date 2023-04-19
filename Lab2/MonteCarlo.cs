776920d090ad1d9fbc262ca18931100ab32318ed
yiksvolro <lostik336@gmail.com>
2023-04-19 16:16:44 +0300

    Добавьте файлы проекта.

---
+++ Lab2/MonteCarlo.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Lab2
+{
+    public class MonteCarlo
+    {
+        private static Random random = new Random();
+        private static List<double> xIn = new List<double>();
+        private static List<double> yIn = new List<double>();
+        private static List<double> xOut = new List<double>();
+        private static List<double> yOut = new List<double>();
+
+        public static double Integrate(Func<double, double> f, double a, double b, int n, bool flag)
+        {
+            double maxVal = Enumerable.Range(0, (int)((b - a) / 0.1) + 1)
+                .Select(i => a + i * 0.1)
+                .Select(x => f(x))
+                .Max();
+            int pointsInside = 0;
+            for (int i = 0; i < n; i++)
+            {
+                double X = GetRandomNumber(a, b);
+                double Y = GetRandomNumber(0, maxVal);
+                if(Y <= f(X))
+                {
+                    pointsInside++;
+                }
+                if (flag && Y <= f(X))
+                {
+                    xIn.Add(X);
+                    yIn.Add(Y);
+                }
+                else if (flag)
+                {
+                    xOut.Add(X);
+                    yOut.Add(Y);
+                }
+            }
+
+            double average = (double)pointsInside / n;
+            double result = (b - a) * maxVal / average;
+
+            return result;
+        }
+
+        private static double GetRandomNumber(double min, double max)
+        {
+            return random.NextDouble() * (max - min) + min;
+        }
+        public static void CreateChart(Func<double, double> f, double a, double b)
+        {
+            Chart chart = new Chart();
+
+            chart.ChartAreas.Add(new ChartArea());
+            chart.Series.Add(new Series("Series1"));
+
+            chart.Series["Series1"].ChartType = SeriesChartType.Point;
+            chart.Series["Series1"].MarkerSize = 5;
+            chart.Series["Series1"].MarkerColor = Color.Green;
+            for (int i = 0; i < xIn.Count; i++)
+            {
+                chart.Series["Series1"].Points.AddXY(xIn[i], yIn[i]);
+            }
+
+            chart.Series.Add(new Series("Series2"));
+            chart.Series["Series2"].ChartType = SeriesChartType.Point;
+            chart.Series["Series2"].MarkerSize = 5;
+            chart.Series["Series2"].MarkerColor = Color.Red;
+            for (int i = 0; i < xIn.Count; i++)
+            {
+                chart.Series["Series2"].Points.AddXY(xOut[i], yOut[i]);
+            }
+
+            chart.Series.Add(new Series("Series3"));
+            chart.Series["Series3"].ChartType = SeriesChartType.Line;
+            chart.Series["Series3"].BorderWidth = 3;
+
+            for (double x = a; x < b; x += 0.01)
+            {
+                chart.Series["Series3"].Points.AddXY(x, f(x));
+            }
+            chart.Size = new Size(1920, 1080);
+            chart.SaveImage("chart.png", ChartImageFormat.Png);
+        
+        }
+    }
+}
---
+++ Lab2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq.Dynamic.Core;
+
+namespace Lab2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Введіть ваше рівняння: ");
+            string expression = Console.ReadLine();
+            var lambda = DynamicExpressionParser.ParseLambda(typeof(double), typeof(double), expression);
+            Func<double, double> func = (Func<double, double>)lambda.Compile();
+            Console.WriteLine("Введіть нижню межу а = ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введіть верхню межу b = ");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введіть кількість точок n = ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            double result1 = MonteCarlo.Integrate(func, a, b, n,true);
+            double result2 = MonteCarlo.Integrate(func, a, b, 100000000,false);
+            Console.WriteLine("I = " + result1);
+            GetErrors(result1, result2);
+            MonteCarlo.CreateChart(func, a, b);
+        }
+        private static void GetErrors(double result, double resultExact)
+        {
+            double errorAbs = 0;
+            double errorRel = 0;
+            if(result != 0)
+            {
+                errorAbs = Math.Abs(resultExact - result);
+                if(resultExact == 0)
+                {
+                    errorRel = 0;
+                }
+                else
+                {
+                    errorRel = errorAbs / Math.Abs(resultExact);    
+                }
+
+            }
+            Console.WriteLine("Абсолютна похибка = " + errorAbs);
+            Console.WriteLine("Відносна похибка = " + errorRel);
+
+
+        }
+    }
+}
